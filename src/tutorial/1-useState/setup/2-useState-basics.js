216e61a79f07f90fa3d0f2225fb00dc9147b9091
Aman Madd <29maddhesiaaman@gmail.com>
2023-06-24 12:19:28 +0530

    Done with logging the statements, Just a knowledge enhancement stuff

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,22 @@
 import React, { useState } from 'react';
-
+// useState - is a function 
+// Just a check
+// console.log("Outer most")
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+// Just a check
+  // console.log("Aman Madd outer")
+return<>
+    <h2>useState basic example</h2>;
+    {/* Just a check */}
+    {/* {
+      console.log("Aman Madd")
+    } */}
+  </> 
+
 };
+
+
+// Just a check
+// console.log("Aman Madd outerlast")
 
 export default UseStateBasics;
