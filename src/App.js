316634584a8e5a6bd05bf23120454a6a94b0c146
Aman Madd <29maddhesiaaman@gmail.com>
2023-06-24 12:06:43 +0530

    going to dive in useState hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Setup from './tutorial/1-useState/setup/1-error-example' 
-
+import Final from './tutorial/1-useState/final/2-useState-basics' 
 
 
 function App() {
   return (
     <div className='container'>
       <Setup/>
-      
+      <Final/>
       {/* <h2>Advanced Tutorial n 12</h2> */}
     </div>
   )
--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -3,14 +3,16 @@ import React from 'react';
 const ErrorExample = () => {
   let title="random title"
   const handleClick=(msg)=>{
-    alert(msg);
+    // title=msg;
+    alert(msg)
+    alert(title)
   }
   return (
     <>
     <h2>
       {title}
     </h2>
-    <button className='btn' onClick={function (){handleClick("Passed msg123")}}>Click me</button>
+    <button className='btn' onClick={function (){handleClick("Passed msg")}}>Click me</button>
     </>
   )
 };
