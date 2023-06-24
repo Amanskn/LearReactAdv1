760655b127f038029c3235a6c74da71e661d602f
Aman Madd <29maddhesiaaman@gmail.com>
2023-06-24 11:48:00 +0530

    Remember: if passing arguments in evenet handler is necessary then you are supposed to invoke that event handler inside an arrow or a normal function , Got it

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 const ErrorExample = () => {
   let title="random title"
+  const handleClick=(msg)=>{
+    alert(msg);
+  }
   return (
     <>
     <h2>
       {title}
     </h2>
-    <button className='btn' onClick={()=>{alert('Hello')}}>Click me</button>
+    <button className='btn' onClick={()=>{handleClick("Passed msg")}}>Click me</button>
     </>
   )
 };
