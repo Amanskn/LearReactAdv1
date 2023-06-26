30fb396bd0a7090756b46013cc539211ea22aeed
Aman Madd <29maddhesiaaman@gmail.com>
2023-06-26 16:45:41 +0530

    Done with passing a function inside state handler function inorder to deal with the latest state value( the most updated one )

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -6,6 +6,22 @@ const UseStateCounter = () => {
     // console.log('called')
     setValue(0);
   }
+
+  const complexIncrease = ()=>{
+    setTimeout(()=>{
+      // Just a check
+      console.log("inside complex increase")
+      // setValue(value+1);
+
+      // passing a function inside  set state function , Why??
+      // Ans:- to take into consideration the latest updated state value, Got it?
+      setValue((previousValue)=>{
+          return previousValue+1;
+      })
+    },2000)
+  }
+
+
   return <>
     <section style={{margin:'4rem 0'}}>
       <h2>regular counter</h2>
@@ -13,6 +29,13 @@ const UseStateCounter = () => {
       <button className='btn' onClick={()=>{setValue(value-1)}}>decrease</button>
       <button className='btn' onClick={resetFunction}>reset</button>
       <button className='btn' onClick={()=>{setValue(value+1)}}>increase</button>
+    </section>
+
+    {/* Going to make more complex counter */}
+    <section style={{margin:'4rem 0'}}>
+      <h2>more complex counter</h2>
+      <h1>{value}</h1>
+      <button className='btn' onClick={complexIncrease}>increase later</button>
     </section>
   </>;
 };
