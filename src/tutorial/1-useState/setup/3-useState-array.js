664d456a3813d57362feed91ba07fb0e82a3d9ac
Aman Madd <29maddhesiaaman@gmail.com>
2023-06-26 14:13:52 +0530

    Implemented the functionality of individual deletion and also the updation of all items button

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -4,30 +4,49 @@ import { data } from '../../../data';
 const UseStateArray = () => {
 
   let [people,setPeople]=React.useState(data);
-  const [btn,setBtn]=React.useState("remove");
+  const [btn,setBtn]=React.useState("remove all items");
   
   
-  const toogleContent=()=>{
-    if(btn=='remove'){
-      setBtn('fetch')
+  const toggleContent=()=>{
+    console.log("Called")
+    if(btn=='remove all items'){
+      setBtn('fetch all items')
       setPeople([])
     }
+    else if(people.length==0 && btn!='fetch all items'){
+      setBtn('fetch all items')
+      // setPeople([])
+    }
     else{
-      setBtn('remove');
+      setBtn('remove all items');
       setPeople(data)
     }
   }
+
+
+  const removeEachPerson = (id)=>{
+    const newPeople = people.filter((person)=>{
+      return person.id!=id
+    })
+    setPeople(newPeople);
+    if(newPeople.length==0){
+      toggleContent();
+    }
+  }
+
+
+
   return <>
   {people.map((person)=>{
     return (
       <div className='item' key={person.id}>
         <h3>{person.name}</h3>
-        <button>Remove</button>
+        <button onClick={()=>{removeEachPerson(person.id)}}>Remove</button>
       </div>
     )
   })}
   <h2>UseState Array example</h2>
-  <button className='btn' onClick={toogleContent}>{btn}</button>
+  <button className='btn' onClick={toggleContent}>{btn}</button>
   </>;
 };
 
