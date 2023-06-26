76118e1d227c0cfed9cbc5edb76f484882a93e7d
Aman Madd <29maddhesiaaman@gmail.com>
2023-06-26 15:49:13 +0530

    Done with useState hook over an object state value

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,27 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  
+  const [person,setPerson]=useState({
+    name:'aman',
+    age:25,
+    message:'random message'
+  });
+  const changeMsg = ()=>{
+    if(person.message=="random message")
+      // overriding the message here, Got it
+      setPerson({...person,message:"hello world!"})
+    else
+    // overriding the message here, Got it
+      setPerson({...person,message:"random message"})
+  }
+  return <>
+    <h2>useState object example</h2>
+    <h3>{"Name :- "+person.name}</h3>
+    <h3>{"Age :- "+person.age}</h3>
+    <h3>{"message :- "+person.message}</h3>
+    <button className='btn' onClick={changeMsg}>Change message</button>
+  </>
 };
 
 export default UseStateObject;
