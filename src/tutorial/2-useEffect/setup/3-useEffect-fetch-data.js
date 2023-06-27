6c6659cbf9be6f08de197dd93f3a792667c6a4f0
Aman Madd <29maddhesiaaman@gmail.com>
2023-06-27 16:48:04 +0530

    useEffect cannot return a promise, remember it

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -3,7 +3,28 @@ import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users';
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
+
+  const [users,setUsers] = useState([]);
+  async function logJSONData() {
+    const response = await fetch(url);
+    const jsonData = await response.json();
+    console.log("The is the data:-",jsonData);
+  }
+
+  // ----------this is the correct one
+  useEffect(()=>{
+    logJSONData();
+  })
+
+  // ------------Please avoid this, because useEffect cannot return a promise and lodJSONData will return a promise why? because it is an async function, Got it
+  // useEffect(logJSONData)
+
+
+
+  
+  return <>
+    <h3>github users</h3>
+  </>
 };
 
 export default UseEffectFetchData;
