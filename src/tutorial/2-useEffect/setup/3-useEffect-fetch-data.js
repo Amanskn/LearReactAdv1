152e0fe39f29d6616d06f8f9aacbeab75d28f96e
Aman Madd <29maddhesiaaman@gmail.com>
2023-06-27 18:21:21 +0530

    Done with fetching the data from github Api

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -7,6 +7,10 @@ const UseEffectFetchData = () => {
   const [users,setUsers] = useState([]);
   
   async function getUsers() {
+    // ----------- A simple loop to visualize that it takes some time to fetch data
+    // for(let i=0;i<10000000000;i++){
+
+    // }
     const response = await fetch(url);
     const users = await response.json();
 
@@ -28,6 +32,24 @@ const UseEffectFetchData = () => {
 
   return <>
     <h3>github users</h3>
+    <ul className="users">
+      {users.map((user)=>{
+        const {avatar_url, html_url,id,login}=user;
+      return (
+        <li key={id}>
+          <img src={avatar_url} alt={login} />
+          <div>
+            <h4>{login}</h4>
+            <a href={html_url}> profile</a>
+          </div>
+
+          
+        </li>
+
+        
+      )
+    })}
+    </ul>
   </>
 };
 
