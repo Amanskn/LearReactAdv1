317318818cd44a9de1f0da57b1342b30c183696f
Aman Madd <29maddhesiaaman@gmail.com>
2023-06-27 17:55:46 +0530

    Passing empty dependency array inside useEffect to avoid infinite loop

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -5,23 +5,27 @@ const url = 'https://api.github.com/users';
 const UseEffectFetchData = () => {
 
   const [users,setUsers] = useState([]);
-  async function logJSONData() {
+  
+  async function getUsers() {
     const response = await fetch(url);
-    const jsonData = await response.json();
-    console.log("The is the data:-",jsonData);
+    const users = await response.json();
+
+    setUsers(users);
+
+    console.log("The is the data:-",users);
   }
 
   // ----------this is the correct one
   useEffect(()=>{
-    logJSONData();
-  })
+    getUsers();
+  },[])            // <<<<----------make sure to pass a dependency array here , else it will go on an infinite loop
 
-  // ------------Please avoid this, because useEffect cannot return a promise and lodJSONData will return a promise why? because it is an async function, Got it
-  // useEffect(logJSONData)
+  // ------------Please avoid this, because useEffect cannot return a promise and getUsers will return a promise why? because it is an async function, Got it
+  // useEffect(getUsers)
 
 
 
-  
+
   return <>
     <h3>github users</h3>
   </>
