137eb1a5f5b442e833c2dd7c51a8c125a738e7b7
Aman Madd <29maddhesiaaman@gmail.com>
2023-06-27 22:29:51 +0530

    NOTE:- errors which come due to status code outside the range of 200 to 299  are not thrown directly into catch block via fetch-api. For this you need to check the status code inside then callback and throw the error accordingly using new Error object and then the thrown error will be gone into the catch block, Got it :), This is one of the limitation of Fetch-Api

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,17 +1,57 @@
+import { toBeRequired } from '@testing-library/jest-dom';
 import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 
 
 
 const MultipleReturns = () => {
-  const [loading,setLoading] = useState(true);
-  console.log("This is the loading value :-",loading)
-  if(loading)
+  const [isLoading,setIsLoading] = useState(true);
+  const [isError,setIsError] = useState(false);
+  const [user,setUser] = useState('default user');
+
+  // console.log("This is the loading value :-",loading)
+  useEffect(()=>{
+    fetch(url)
+    .then((resp)=>{
+      if(resp.status>=200 && resp.status<=299)
+        return resp.json()
+      else{
+        setIsLoading(false);
+        setIsError(true)
+        throw new Error(`No data fetched and the status code is:- ${resp.status}`)
+        // console.log(first)
+      }
+    })
+    .then((user)=>{
+      // A simple check
+      // for(let i=0;i<10000000;i++)
+
+      const {login}=user;
+      setUser(login);
+      setIsLoading(false); 
+    })
+    .catch((err)=>{
+      console.log("An Error came:-",err)
+    })
+  },[])
+
+
+  if(isLoading){
+
     return <>
       <h2>Loading...</h2>
-      <button className='btn' onClick={()=>{setLoading(false)}}>load content</button>
+      
     </>;
-  return <h2>Loaded</h2>;
+  }
+  else if (isError){
+    return <>
+    <h2>Error...</h2>
+    </>
+  }
+  else{
+
+    return <h2>{user}</h2>;
+  }
 };
 
 export default MultipleReturns;
