eb28438f9711ad6e103825dfb19e86f71f4b443c
Aman Madd <29maddhesiaaman@gmail.com>
2023-06-28 12:09:25 +0530

    setup the toggle state using &&

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,14 +3,29 @@ import React, { useState } from 'react';
 // ternary operator
 
 const ShortCircuit = () => {
-  const [text,setText]=useState('k');
-  const firstValue = text || 'hello world';
-  const secondValue = text && 'hello world'&& "Aman";
+  const [text,setText]=useState('h');
+
+  const [isError,setIsError] = useState(false);
+  // const firstValue = text || 'hello world';
+  // const secondValue = text && 'hello world'&& "Aman";
 
   return <>
 
-    <h1>firstValue is :-{firstValue}</h1>
-    <h1>secondValue is :-{secondValue}</h1>
+    <h1>{text || "Alternate text"}</h1>
+    <button className='btn' onClick={()=>{setIsError(!isError)}}>Toggle error</button>
+    {isError && <h1>error...</h1> }
+
+
+
+
+    {/* <h2>{text && "I will be shown if first text is there"}</h2>
+    <h2>{!text && "I will be shown if first text is not there"}</h2>
+    <h2>{text && <b>I am inside a component and the component will be rendered iff first text is present</b> }</h2> */}
+    
+    
+    
+    {/* <h1>firstValue is :-{firstValue}</h1>
+    <h1>secondValue is :-{secondValue}</h1> */}
      
   
     {/* ----------Error:- Why? because inside return there cannot be if block */}
