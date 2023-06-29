b151f13bcb0fe5bd0db588c702f87f66b6494f56
Aman Madd <29maddhesiaaman@gmail.com>
2023-06-29 18:22:34 +0530

    Done with multiple-inputs

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import React from 'react'
-import Setup from './tutorial/4-forms/setup/1-controlled-inputs' 
+import React from "react";
+import Setup from "./tutorial/4-forms/setup/2-multiple-inputs";
 
-// import Final from './tutorial/2-useEffect/final/3-useEffect-fetch-data' 
-
-
-
-
+// import Final from './tutorial/2-useEffect/final/3-useEffect-fetch-data'
 
 function App() {
   return (
-    <div className='container'>
-      <Setup/>
-      
+    <div className="container">
+      <Setup />
+
       {/* <Final/> */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
