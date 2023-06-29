e04cdc046ca01920af6b906f85f2a163f5be0b1f
Aman Madd <29maddhesiaaman@gmail.com>
2023-06-29 19:47:48 +0530

    Done with useRef hook

--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -1,11 +1,42 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
 
-// preserves value
-// DOES NOT trigger re-render
-// target DOM nodes/elements
+// preserves value (this is the similarity of useRef hook with useState hook)
+// DOES NOT trigger re-render (this is the difference of useRef hook with useState hook)
+// useRef is mostly used to target DOM nodes/elements
 
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+  const inputContainer = useRef(null);
+  const divContainer = useRef(null);
+  const btnContainer = useRef(null);
+  console.log(inputContainer, "This is the value of inputContainer outside");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(
+      inputContainer,
+      "This is the value of inputContainer on submission"
+    );
+    console.log(btnContainer.current);
+    console.log(divContainer.current);
+  };
+
+  useEffect(() => {
+    console.log("useEffect ran");
+
+    // --------this is to focus on the input tag as soon as the page loads
+    inputContainer.current.focus();
+  }, []);
+  return (
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <div>
+          <input type="text" ref={inputContainer} />
+          <button ref={btnContainer}>Submit </button>
+        </div>
+      </form>
+      <div ref={divContainer}>Simple div</div>
+    </>
+  );
 };
 
 export default UseRefBasics;
